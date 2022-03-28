0c96c46f5cb30e567fb601fce58ecb16a2741672
Giulia-92 <cionco.giulia@gmail.com>
2022-03-28 22:02:50 +0200

    js-fizzbuzzdom

--- style.js
+++ style.js
@@ -3,32 +3,38 @@ const cesto = document.getElementById("cesto");
 const linea = document.createElement("div");
 linea.setAttribute("class", "row g-3 mt-4");
 cesto.append(linea);
-for (let numero = 1; numero <= 25; numero ++){
+for (let numero = 1; numero <= 100; numero ++){
 
  if (numero % 3 == 0){
         const colonna2 = document.createElement("div");
         colonna2.setAttribute("class", "color-2");
         const colonna = document.createElement("div");
-        colonna.setAttribute("class", "col-12 col-sm-6 col-md-4 col-lg-2 my-4");        
+        colonna.setAttribute("class", "col-12");        
         const Testo = document.createTextNode("Fizz");
-        colonna.append(colonna2);
-        colonna2.append(Testo);
-        linea.append(colonna);
+        colonna2.append(colonna);
+        colonna.append(Testo);
+        linea.append(colonna2);
         console.log("Fizz");
     }
  else if (numero % 5== 0){
-        const colonna = document.createElement("div");
+        const colonna2 = document.createElement("div");
         colonna.setAttribute("class", "color-3");
+        const colonna = document.createElement("div");
+        colonna.setAttribute("class", "col-12");       
         const textNode = document.createTextNode("Buzz");
+        colonna2.append(colonna);
         colonna.append(textNode);
-        linea.append(colonna);
+        linea.append(colonna2);
         console.log("Buzz");}  
  else if ((numero % 3 == 0) && (numero % 5 == 0)) {
-        const colonna = document.createElement("div");
+        const colonna2 = document.createElement("div");
         colonna.setAttribute("class", "color-4");
+        const colonna = document.createElement("div");
+        colonna.setAttribute("class", "col-12");       
         const textNode = document.createTextNode("FizzBuzz");
+        colonna2.append(colonna);
         colonna.append(textNode);
-        linea.append(colonna);
+        linea.append(colonna2);
         console.log("FizzBuzz")
  } else{        
         const colonna = document.createElement("div");
