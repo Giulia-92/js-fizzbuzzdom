bfdd676caa5481f90d43bfc590469585ea1b166a
Giulia-92 <cionco.giulia@gmail.com>
2022-03-29 10:06:03 +0200

    js-fizzbuzzdom

--- style.js
+++ style.js
@@ -3,50 +3,40 @@ const cesto = document.getElementById("cesto");
 const linea = document.createElement("div");
 linea.setAttribute("class", "row g-3 mt-4");
 cesto.append(linea);
-for (let numero = 1; numero <= 100; numero ++){
+for (let numero = 1; numero <= 100; numero++){
+    const colonna = document.createElement("div");
+    colonna.setAttribute("class", "col-12 col-sm-6 col-md-4 col-lg-2");
+    const colonna2 = document.createElement("div");
+    let Testo;
 
- if (numero % 3 == 0){
-        const colonna2 = document.createElement("div");
-        colonna2.setAttribute("class", "color-2");
-        const colonna = document.createElement("div");
-        colonna.setAttribute("class", "col-12");        
-        const Testo = document.createTextNode("Fizz");
-        colonna2.append(colonna);
-        colonna.append(Testo);
-        linea.append(colonna2);
-        console.log("Fizz");
+ if((numero % 3 == 0) && (numero % 5 == 0)) {
+    colonna2.setAttribute("class", "color-4");      
+    Testo = document.createTextNode("FizzBuzz");
+    console.log("FizzBuzz")
+
     }
  else if (numero % 5== 0){
-        const colonna2 = document.createElement("div");
-        colonna.setAttribute("class", "color-3");
-        const colonna = document.createElement("div");
-        colonna.setAttribute("class", "col-12");       
-        const textNode = document.createTextNode("Buzz");
-        colonna2.append(colonna);
-        colonna.append(textNode);
-        linea.append(colonna2);
+        colonna2.setAttribute("class", "color-3");      
+        Testo = document.createTextNode("Buzz");
         console.log("Buzz");}  
- else if ((numero % 3 == 0) && (numero % 5 == 0)) {
-        const colonna2 = document.createElement("div");
-        colonna.setAttribute("class", "color-4");
-        const colonna = document.createElement("div");
-        colonna.setAttribute("class", "col-12");       
-        const textNode = document.createTextNode("FizzBuzz");
-        colonna2.append(colonna);
-        colonna.append(textNode);
-        linea.append(colonna2);
-        console.log("FizzBuzz")
+ else if 
+ (numero % 3 == 0){
+    
+    colonna2.setAttribute("class", "color-2");        
+     Testo = document.createTextNode("Fizz");
+    console.log("Fizz");
  } else{        
-        const colonna = document.createElement("div");
-        colonna.setAttribute("class", "color-1");
-        const textNode = document.createTextNode(numero);
-        colonna.append(textNode);
-        linea.append(colonna);
-        console.log(numero);}     
+    
+        colonna2.setAttribute("class", "color-1");
+        Testo = document.createTextNode(numero);
+        
+        console.log(numero);
+    }     
 
-    }
-
-
+    colonna2.append(Testo);
+    colonna.append(colonna2);
+    linea.append(colonna);
+}
 
 
     /*if (numero % 3 == 0) && (numero % 5 == 0);{
