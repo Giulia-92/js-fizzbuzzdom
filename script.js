f3498aaad2622aca1f08ea911f50776bc4950987
Giulia-92 <cionco.giulia@gmail.com>
2022-03-29 10:56:46 +0200

    js-fizzbuzzdom

--- script.js
+++ script.js
@@ -3,35 +3,34 @@ const cesto = document.getElementById("cesto");
 const linea = document.createElement("div");
 linea.setAttribute("class", "row g-3 mt-4");
 cesto.append(linea);
-for (let numero = 1; numero <= 100; numero++){
+for (let numero = 1; numero <= 100; numero++) {
     const colonna = document.createElement("div");
     colonna.setAttribute("class", "col-12 col-sm-6 col-md-4 col-lg-2");
     const colonna2 = document.createElement("div");
     let Testo;
 
- if((numero % 3 == 0) && (numero % 5 == 0)) {
-    colonna2.setAttribute("class", "color-4");      
-    Testo = document.createTextNode("FizzBuzz");
-    console.log("FizzBuzz")
+    if ((numero % 3 == 0) && (numero % 5 == 0)) {
+        colonna2.setAttribute("class", "color-4");
+        Testo = document.createTextNode("FizzBuzz");
+        console.log("FizzBuzz")
 
     }
- else if (numero % 5== 0){
-        colonna2.setAttribute("class", "color-3");      
+    else if (numero % 5 == 0) {
+        colonna2.setAttribute("class", "color-3");
         Testo = document.createTextNode("Buzz");
-        console.log("Buzz");}  
- else if 
- (numero % 3 == 0){
-    
-    colonna2.setAttribute("class", "color-2");        
-     Testo = document.createTextNode("Fizz");
-    console.log("Fizz");
- } else{        
-    
+        console.log("Buzz");
+    }
+    else if
+        (numero % 3 == 0) {
+        colonna2.setAttribute("class", "color-2");
+        Testo = document.createTextNode("Fizz");
+        console.log("Fizz");
+    } else {
+
         colonna2.setAttribute("class", "color-1");
         Testo = document.createTextNode(numero);
-        
         console.log(numero);
-    }     
+    }
 
     colonna2.append(Testo);
     colonna.append(colonna2);
@@ -39,6 +38,6 @@ for (let numero = 1; numero <= 100; numero++){
 }
 
 
-    /*if (numero % 3 == 0) && (numero % 5 == 0);{
-        console.log("FizzBuzz")
-    } */     
+/*if (numero % 3 == 0) && (numero % 5 == 0);{
+    console.log("FizzBuzz")
+} */
